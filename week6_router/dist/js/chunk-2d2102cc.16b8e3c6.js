a760bf53cbf5ce5a14f894d3064683dec651543a
饒rao <jiahungrao@gmail.com>
2020-08-05 17:20:37 +0800

    p

---
+++ week6_router/dist/js/chunk-2d2102cc.16b8e3c6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2102cc"],{b789:function(t,n,c){"use strict";c.r(n);var e=function(){var t=this,n=t.$createElement,c=t._self._c||n;return c("div",[c("h1",[t._v("購物車")]),c("router-link",{attrs:{to:"/cart/checkout"}},[t._v("前往結帳")])],1)},r=[],u={},l=u,o=c("2877"),a=Object(o["a"])(l,e,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d2102cc.16b8e3c6.js.map
